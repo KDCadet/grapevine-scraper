cfb3c36e2d6a9b39a7174534b37c96095da6c948
Igor Semyonov <igor@semyonov.xyz>
2022-06-05 13:36:19 -0400

    correctly logging in and fetching each article

--- scraper.py
+++ scraper.py
@@ -1,26 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-cookies = {
-    'SSESS4cae9a55eb3358ae812f45d860b25d2f': 'bYVNfjhKylR1obHpnmylePX%2C58V%2C5dXBg57zkVCmaYdTexwF',
-}
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
-    'Accept': 'image/webp,*/*',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
+    'Origin': 'https://www.aagrapevine.org',
     'Connection': 'keep-alive',
-    'Referer': 'https://www.aagrapevine.org/magazine',
-    # 'Cookie': 'SSESS4cae9a55eb3358ae812f45d860b25d2f=bYVNfjhKylR1obHpnmylePX%2C58V%2C5dXBg57zkVCmaYdTexwF',
-    'Sec-Fetch-Dest': 'image',
-    'Sec-Fetch-Mode': 'no-cors',
+    'Referer': 'https://www.aagrapevine.org/user/login',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Site': 'same-origin',
-    'Cache-Control': 'max-age=0',
+    'Sec-Fetch-User': '?1',
     # Requests doesn't support trailers
     # 'TE': 'trailers',
 }
 
-page = requests.get('https://www.aagrapevine.org/magazine', cookies=cookies, headers=headers)
+with open('user.name', 'r') as f:
+    userName = f.readline()
+with open('user.pass', 'r') as f:
+    userPass = f.readline()
+
+data = {
+    'name': userName,
+    'pass': userPass,
+    'form_build_id': 'form-3LnHtbxBJ2X1VWbuUQHTQ4qv2y3WEkBr4V0ywmhGmQ0',
+    'form_id': 'user_login_form',
+    'op': 'Log in',
+}
+
+s = requests.session()
+response = s.post('https://www.aagrapevine.org/user/login', headers=headers, data=data)
+
+
+page = s.get('https://www.aagrapevine.org/magazine')#, cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
@@ -42,10 +56,15 @@ result = soup.find(class_='field field--name-name field--type-string field--labe
 
 
 
-article_page = requests.get(article_urls[0], cookies=cookies, headers=headers)
+for article_url in article_urls:
+    article_page = s.get(article_url)
 
-soup = BeautifulSoup(article_page.content, "html.parser")
+    soup = BeautifulSoup(article_page.content, "html.parser")
 
-results = soup.find_all(class_='clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item')
+    results = soup.find_all(class_='clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item')
 
-print(results[0])
+    for element in results:
+        element_results = element.find_all('p')
+        print(len(element_results))
+    print('_' * 50)
+s.close()
